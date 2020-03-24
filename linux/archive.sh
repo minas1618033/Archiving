855e48e4a86cc870aea65d88e81d2a8978f0fc22
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:46:48 +0800

    2020-01-13 v2.4.0
    Add : check filename to avoid empty collection

--- linux/archive.sh
+++ linux/archive.sh
@@ -2,18 +2,21 @@
 # !/bin/bash
 cd ~/Downloads/HV.2/A-Class/
 for filename in *.jpg; do
-    wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.A-Class/ ~/Downloads/HV.3/A-Class/${filename%%.*}.7z ${filename%%.*}*.*
-    mv ${filename%%.*}*.jpg ~/Downloads/HV.3/A-Class/
-done
+    if [ ${#filename} -ge 7 ]; then
+        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.A-Class/ ~/Downloads/HV.3/A-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        mv $filename ~/Downloads/HV.3/A-Class/
+    fi done
 
 cd ~/Downloads/HV.2/B-Class/
 for filename in *.jpg; do
-    wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.B-Class/ ~/Downloads/HV.3/B-Class/${filename%%.*}.7z ${filename%%.*}*.*
-    mv ${filename%%.*}*.jpg ~/Downloads/HV.3/B-Class/
-done
+    if [ ${#filename} -ge 7 ]; then
+        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.B-Class/ ~/Downloads/HV.3/B-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        mv $filename ~/Downloads/HV.3/B-Class/
+    fi done
 
 cd ~/Downloads/HV.2/C-Class/
 for filename in *.jpg; do
-    wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.C-Class/ ~/Downloads/HV.3/C-Class/${filename%%.*}.7z ${filename%%.*}*.*
-    mv ${filename%%.*}*.jpg ~/Downloads/HV.3/C-Class/
-done
+    if [ ${#filename} -ge 7 ]; then
+        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.C-Class/ ~/Downloads/HV.3/C-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        mv $filename ~/Downloads/HV.3/C-Class/
+    fi done
