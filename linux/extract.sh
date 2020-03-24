248f98045749b029990217e7deaecbddff47231e
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:21:22 +0800

    2020-01-20 v1.2.0 (unstable)
    Modify : use <find> to replace <for> loop

--- linux/extract.sh
+++ linux/extract.sh
@@ -2,9 +2,6 @@
 # !/bin/bash
 mv ~/Downloads/HV.1/*.rar ~/Downloads/HV.2/.Temp/
 cd ~/Downloads/HV.2/
-for filename in .Temp/*.rar;
-  do
-    unrar e -c- -cu -o- -pPASSWORD "${filename}" \*.{avi,AVI,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV};
-  done
-rm .Temp/*.rar
+find . -name '.Temp/*.rar' -exec totalsize="$(du -sh | awk '{print $1}')" && echo "$totalsize is Pending" \; -exec unrar e -c- -cu -o- -pPASSWORD {} \*.{avi,AVI,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV} \; -exec rm {} \;
 find . -name '*.*' -type f -exec bash -c 'base=${0%.*} ext=${0##*.} a=$base.${ext,,}; [ "$a" != "$0" ] && mv -- "$0" "$a"' {} \;
+rm .Temp/*.rar
