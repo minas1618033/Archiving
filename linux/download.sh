5ecfbc2f1bfdf58e269deb5706fac3fe00f68352
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:07:57 +0800

    2020-03-23 v1.3.0
    Add : run extract script after download finished

--- linux/download.sh
+++ linux/download.sh
@@ -1,7 +1,21 @@
 # Download files from Google Drive by rclone -----------------------------------
 # !/bin/bash
-clear
-rclone move --ignore-existing --transfers 1 --use-mmap --buffer-size 1G  --progress --stats-file-name-length 0 GSuite:/1 ~/Downloads/HV.1/.Downloading/
-rclone move --include "/*.rar" --ignore-existing --transfers 1 --use-mmap --buffer-size 1G  --progress --stats-file-name-length 0 GSuite:/ ~/Downloads/HV.1/.Downloading/
-mv ~/Downloads/HV.1/.Downloading/* ~/Downloads/HV.1/
+mv ~/Downloads/HVideo/HV.1/c/* ~/Downloads/HVideo/HV.2/
+rclone move --include "/*.rar" --ignore-existing --transfers 1 --use-mmap --buffer-size 1G  --progress --stats-file-name-length 0 GSuite:/ ~/Downloads/HVideo/HV.1/.d/
+mv ~/Downloads/HVideo/HV.1/.d/* ~/Downloads/HVideo/HV.1/
 printf "\n   << Download Finished >>\n\n"
+
+total=1
+count=0
+while [ ${count} -lt ${total} ] ; do
+    tlimit=$(( $total - $count ))
+    echo -e "\rRun extract.sh directly or end the process? (E)xtract / (N)o \c"
+    read -n1 -t 360 RESPONSE
+    test ! -z "$RESPONSE" && { break ; }
+    count=$((count+1))
+done
+case $RESPONSE in
+    [eEyY]) sh ~/Documents/scripts/extract.sh ;;
+    [nN]) printf "\n\n(OK) Checking Done\n\n" ;;
+    *) sh ~/Documents/scripts/extract.sh ;;
+esac
