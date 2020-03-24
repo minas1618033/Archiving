cd517d2f8f836beaa1d6d2ae877e6e79ccfa0afd
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:48:40 +0800

    2020-01-21 v2.4.1
    Modify : use "WINEDEBUG=fixme-all" to ignore wine warning
    Add : use clear command

--- linux/archive.sh
+++ linux/archive.sh
@@ -1,22 +1,23 @@
 # Archive -----------------------------------
 # !/bin/bash
+clear
 cd ~/Downloads/HV.2/A-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
-        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.A-Class/ ~/Downloads/HV.3/A-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.A-Class/ ~/Downloads/HV.3/A-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/A-Class/
     fi done
 
 cd ~/Downloads/HV.2/B-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
-        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.B-Class/ ~/Downloads/HV.3/B-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.B-Class/ ~/Downloads/HV.3/B-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/B-Class/
     fi done
 
 cd ~/Downloads/HV.2/C-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
-        wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/zelko/Downloads/HV.3/.C-Class/ ~/Downloads/HV.3/C-Class/${filename%%.*}.7z ${filename%%.*}*.*
+        WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.C-Class/ ~/Downloads/HV.3/C-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/C-Class/
     fi done
