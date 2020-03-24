2b61d248757717146c7e3cc7891e1f1860594975
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:30:43 +0800

    2020-03-23 v1.4.1
    Modify : replace some words

--- linux/extract.sh
+++ linux/extract.sh
@@ -1,21 +1,20 @@
 # Extract RAR archives
 # !/bin/bash
-clear
-cd ~/Downloads/HV.2/
-ls ~/Downloads/HV.1/ | while read illegalname
+cd ~/Downloads/HVideo/HV.2/
+ls ~/Downloads/HVideo/HV.1/ | while read filename
 do
-   mv ~/Downloads/HV.1/"$illegalname" .Temp/"${illegalname#*of }"
+   mv ~/Downloads/HVideo/HV.1/"$filename" ~/Downloads/HVideo/HV.1/.e/"${filename#*of }"
 done
-
-for rarfile in .Temp/*.rar; do
-    remainsize1="$(du -sh ~/Downloads/HV.2/.Temp/ | awk '{print $1}')"
-    remainsize2="$(du -s ~/Downloads/HV.2/.Temp/ | awk '{print $1}')"
-    transferspeed=50000
-    ETA1=$((remainsize2/transferspeed)) 
+mv ~/Downloads/HVideo/HV.1/.e/c ~/Downloads/HVideo/HV.1/c
+for RARFILE in ~/Downloads/HVideo/HV.1/.e/*.rar; do
+    size1="$(du -sh ~/Downloads/HVideo/HV.1/.e/ | awk '{print $1}')"
+    size2="$(du -s ~/Downloads/HVideo/HV.1/.e/ | awk '{print $1}')"
+    rate=100000
+    ETA1=$((size2/rate)) 
     ETA2="$(date -d@$ETA1 -u +%H:%M:%S)" 
-    printf "\n   << remain $remainsize1 , ETA $ETA2 >>  ----------------------------\n"
-    unrar e -c- -cu -o- -pPASSWORD "${rarfile}" \*.{avi,AVI,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV}
-    rm ${rarfile}
+    printf "\n   << remain $size1 , ETA $ETA2 >>  ----------------------------\n"
+    unrar e -c- -cu -o- -pPASSWORD "${RARFILE}" \*.{avi,AVI,iso,ISO,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,nrg,NRG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV} &&
+    rm ${RARFILE}
 done
 find . -name '*.*' -type f -exec bash -c 'base=${0%.*} ext=${0##*.} a=$base.${ext,,}; [ "$a" != "$0" ] && mv -- "$0" "$a"' {} \;
 printf "\n   << Extraction Finished >>\n\n"
