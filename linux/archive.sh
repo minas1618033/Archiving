6246d4b6825cee23f81fd6020366ff4e762dbbda
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:37:58 +0800

    2019-01-08 v2.0.0
    Modify : merge each extension actions

--- linux/archive.sh
+++ linux/archive.sh
@@ -1,54 +1,17 @@
 # Archive -----------------------------------
 # !/bin/bash
-cd /home/zelko/Downloads/3.Pending/
-mv *.avi .avi
-mv *.mkv .mkv
-mv *.mp4 .mp4
-mv *.mpg .mpg
-mv *.mpeg .mpeg
-mv *.rm .rm
-mv *.rmvb .rmvb
-mv *.vob .vob
-mv *.wmv .wmv
-cd /home/zelko/Downloads/3.Pending/.avi/
-for filename in *.avi; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.mkv/
-for filename in *.mkv; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.mp4/
-for filename in *.mp4; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.mpg/
-for filename in *.mpg; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.mpeg/
-for filename in *.mpeg; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.rm/
-for filename in *.rm; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.rmvb/
-for filename in *.rmvb; do 7z a -t7z -m0=lzma2 -mx1 -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.vob/
-for filename in *.vob; do 7z a -t7z -m0=lzma2 -mx1 --mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-cd /home/zelko/Downloads/3.Pending/.wmv/
-for filename in *.wmv; do 7z a -t7z -m0=lzma2 -mx1 --mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; done;
-mv *.7z ../
-
-# Method 1, when for function have Null value (If haven't *.avi in folder), then occurre unexpected result.
-# cd /home/zelko/Downloads/3.Pending/
-# for filename in *.{avi,mkv,mp4,mpg,mpeg,rm,rmvb,vob,wmv};
-# do 7z a -t7z -m0=lzma2 -mx1 -stx7z -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename >> ~/Desktop/7z.log;
-# done
-
-# Method 2, still can't execute.
-# null=;
-# for filename in *.{avi,mkv,mp4,mpg,mpeg,rm,rmvb,vob,wmv};
-# do 
-# if [ "${filename}" != "${null}" ]; then
-#	7z a -t7z -m0=lzma2 -mx1 -stx7z -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename;
-# fi
-# done
+cd ~/Downloads/HV.2/A-Class/
+mv *.jpg ~/Downloads/HV.3/A-Class/
+mv *.png ~/Downloads/HV.3/A-Class/
+for filename in *.{avi,mkv,mp4,mpg,mpeg,rm,rmvb,vob,wmv};
+do wine 7z a -t7z -m0=lzma2 -mx9 -stx7z -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; rm "*.7z"; mv *.7z ~/Downloads/HV.3/A-Class/; done
+cd ~/Downloads/HV.2/B-Class/
+mv *.jpg ~/Downloads/HV.3/B-Class/
+mv *.png ~/Downloads/HV.3/B-Class/
+for filename in *.{avi,mkv,mp4,mpg,mpeg,rm,rmvb,vob,wmv};
+do wine 7z a -t7z -m0=lzma2 -mx9 -stx7z -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; rm "*.7z"; mv *.7z ~/Downloads/HV.3/B-Class/; done
+cd ~/Downloads/HV.2/C-Class/
+mv *.jpg ~/Downloads/HV.3/C-Class/
+mv *.png ~/Downloads/HV.3/C-Class/
+for filename in *.{avi,mkv,mp4,mpg,mpeg,rm,rmvb,vob,wmv};
+do wine 7z a -t7z -m0=lzma2 -mx9 -stx7z -mhe -mmt=4 -sdel -p'PASSWORD' ${filename%%.*}.7z $filename; rm "*.7z"; mv *.7z ~/Downloads/HV.3/C-Class/; done
