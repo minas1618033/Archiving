3e7df3bd1d6f03a7174c5492054796c3caf682d2
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 18:03:28 +0800

    2020-01-20 v1.1.0
    Modify : delete default parameters
    Modify : add parameters --no-traverse
    Modify : add parameters --drive-chunk-size
    Modify : add parameters --progress
    Add : show finish message
    Add : use clear command

--- linux/upload.sh
+++ linux/upload.sh
@@ -1,6 +1,20 @@
 # !/bin/bash
-/usr/bin/rclone move --ignore-existing --verbose --transfers 1 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 15s --stats-file-name-length 0 ~/Downloads/HV.3/A-Class/ GSuite:/Video/HVideo/A-Class/
+# Upload Files to GSuite with bandwidth limit-----------------------------------
+clear
+rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/A-Class/ GSuite:/Video/HVideo/A-Class/
+rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
+rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
 
-/usr/bin/rclone move --ignore-existing --verbose --transfers 1 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 15s --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
+printf "\n              << Upload Finished >>\n\n"
 
-/usr/bin/rclone move --ignore-existing --verbose --transfers 1 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 15s --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
+
+# Upload Files to GSuite without bandwidth limit-----------------------------------
+# !/bin/bash
+clear
+rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/A-Class/ GSuite:/Video/HVideo/A-Class/
+
+rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
+
+rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
+
+printf "\n              << Upload Finished >>\n\n"
