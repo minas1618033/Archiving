3fb12bfe75495e68c3e72d413369013d7b57bd30
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:50:05 +0800

    2020-01-24 v2.5.0
    Add : show remain size & ETA

--- linux/archive.sh
+++ linux/archive.sh
@@ -4,6 +4,12 @@ clear
 cd ~/Downloads/HV.2/A-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
+        remainsize1="$(du -ch ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        remainsize2="$(du -c ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        transferspeed=7000
+        ETA1=$((remainsize2/transferspeed))
+        ETA2="$(date -d@$ETA1 -u +%H:%M:%S)" 
+        printf "\n   << remain $remainsize1 , ETA $ETA2 >>  ----------------------------\n"    
         WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.A-Class/ ~/Downloads/HV.3/A-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/A-Class/
     fi done
@@ -11,6 +17,12 @@ for filename in *.jpg; do
 cd ~/Downloads/HV.2/B-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
+        remainsize1="$(du -ch ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        remainsize2="$(du -c ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        transferspeed=7000
+        ETA1=$((remainsize2/transferspeed))
+        ETA2="$(date -d@$ETA1 -u +%H:%M:%S)" 
+        printf "\n   << remain $remainsize1 , ETA $ETA2 >>  ----------------------------\n"    
         WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.B-Class/ ~/Downloads/HV.3/B-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/B-Class/
     fi done
@@ -18,6 +30,12 @@ for filename in *.jpg; do
 cd ~/Downloads/HV.2/C-Class/
 for filename in *.jpg; do
     if [ ${#filename} -ge 7 ]; then
+        remainsize1="$(du -ch ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        remainsize2="$(du -c ~/Downloads/HV.2/A-Class/ ~/Downloads/HV.2/B-Class/ ~/Downloads/HV.2/C-Class/ | tail -n 1 | awk '{print $1}')"
+        transferspeed=7000
+        ETA1=$((remainsize2/transferspeed))
+        ETA2="$(date -d@$ETA1 -u +%H:%M:%S)" 
+        printf "\n   << remain $remainsize1 , ETA $ETA2 >>  ----------------------------\n"    
         WINEDEBUG=fixme-all wine 7z a -mx9 -slp -mmt=4 -mhe -ms=on -p'PASSWORD' -sdel -x!*.jpg -w/home/user/Downloads/HV.3/.C-Class/ ~/Downloads/HV.3/C-Class/${filename%%.*}.7z ${filename%%.*}*.*
         mv $filename ~/Downloads/HV.3/C-Class/
     fi done
