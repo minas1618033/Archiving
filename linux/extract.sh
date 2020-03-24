d599fe6ca50d6010748b4a7777e91b0511827bd3
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 17:14:56 +0800

    2019-12-xx v1.1.0
    Modify : merge "Move" & "Extract" actions

--- linux/extract.sh
+++ linux/extract.sh
@@ -1,20 +1,10 @@
 # Extract RAR archives
 # !/bin/bash
-# 1.Move --------------------------------------
-cd ~/Downloads/1.Downloading/
-for filename in *.*; do mv -- "$filename" "${filename:0: -4}"; done
-mv *.rar ~/Downloads/2.Extracting/
-
-# 2.Extract -----------------------------------
-# !/bin/bash
-cd ~/Downloads/2.Extracting/
-for filename in *.rar;
-do
-  unrar e -cu -o- -pPASSWORD "${filename}" \*.{avi,AVI,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV};
-done
-rm *.rar
+mv ~/Downloads/HV.1/*.rar ~/Downloads/HV.2/.Temp/
+cd ~/Downloads/HV.2/
+for filename in .Temp/*.rar;
+  do
+    unrar e -c- -cu -o- -pPASSWORD "${filename}" \*.{avi,AVI,mkv,MKV,mp4,MP4,mpg,MPG,mpeg,MPEG,rm,RM,rmvb,RMVB,vob,VOB,wmv,WMV};
+  done
+rm .Temp/*.rar
 find . -name '*.*' -type f -exec bash -c 'base=${0%.*} ext=${0##*.} a=$base.${ext,,}; [ "$a" != "$0" ] && mv -- "$0" "$a"' {} \;
-
-# 1.-df (delete file after extracting) maybe invalid in RAR 5.80, so add rm command.
-# 2.Alternative script:
-#	find . -name '*.*' -exec sh -c ' a=$(echo "$0" | sed -r "s/([^.]*)\$/\L\1/"); [ "$a" != "$0" ] && mv "$0" "$a" ' {} \;
