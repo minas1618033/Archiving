038aaa0c408f9b771dc1b342bba5bccb6b2e2f29
Zelko Rocha <47934926+minas1618033@users.noreply.github.com>
2020-03-24 18:05:17 +0800

    2020-01-21 v1.2.0
    Add : add discriminant to ignore empty source

--- linux/upload.sh
+++ linux/upload.sh
@@ -1,20 +1,57 @@
 # !/bin/bash
 # Upload Files to GSuite with bandwidth limit-----------------------------------
 clear
+sourceA="$(du -s ~/Downloads/HV.3/A-Class/ | awk '{print $1}')"
+if [ $sourceA -gt 1000 ]
+then
 rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/A-Class/ GSuite:/Video/HVideo/A-Class/
-rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
-rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
+printf "\n   << A-Class Upload Finished >>\n\n"
+else
+printf "\n   << A-Class Nothing to Do >>\n\n"
+fi
 
-printf "\n              << Upload Finished >>\n\n"
+sourceB="$(du -s ~/Downloads/HV.3/B-Class/ | awk '{print $1}')"
+if [ $sourceB -gt 1000 ]
+then
+rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
+printf "\n   << B-Class Upload Finished >>\n\n"
+else
+printf "\n   << B-Class Nothing to Do >>\n\n"
+fi
+
+sourceB="$(du -s ~/Downloads/HV.3/C-Class/ | awk '{print $1}')"
+if [ $sourceB -gt 1000 ]
+then
+rclone move --ignore-existing --transfers 1 --bwlimit 4M --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
+fi
+printf "\n   << Upload Finished >>\n\n"
+
 
 
 # Upload Files to GSuite without bandwidth limit-----------------------------------
 # !/bin/bash
 clear
+sourceA="$(du -s ~/Downloads/HV.3/A-Class/ | awk '{print $1}')"
+if [ $sourceA -gt 1000 ]
+then
 rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/A-Class/ GSuite:/Video/HVideo/A-Class/
+printf "\n   << A-Class Upload Finished >>\n\n"
+else
+printf "\n   << A-Class Nothing to Do >>\n\n"
+fi
 
+sourceB="$(du -s ~/Downloads/HV.3/B-Class/ | awk '{print $1}')"
+if [ $sourceB -gt 1000 ]
+then
 rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/B-Class/ GSuite:/Video/HVideo/B-Class/
+printf "\n   << B-Class Upload Finished >>\n\n"
+else
+printf "\n   << B-Class Nothing to Do >>\n\n"
+fi
 
+sourceB="$(du -s ~/Downloads/HV.3/C-Class/ | awk '{print $1}')"
+if [ $sourceB -gt 1000 ]
+then
 rclone move --ignore-existing --transfers 1 --no-traverse --drive-chunk-size 1G --progress --stats-file-name-length 0 ~/Downloads/HV.3/C-Class/ GSuite:/Video/HVideo/C-Class/
-
-printf "\n              << Upload Finished >>\n\n"
+fi
+printf "\n   << Upload Finished >>\n\n"
